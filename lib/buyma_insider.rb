65682165bbc9ca3bbbf13bbf0ab010bc0d78b606
Charles Yang <donchoa@gmail.com>
2016-08-26 07:55:34 -0400

    Queue working after reinstall everything in vendor

--- lib/buyma_insider.rb
+++ lib/buyma_insider.rb
@@ -1,6 +1,6 @@
 require 'require_all'
 
-require_all 'buyma_insider/concerns'
+require_all 'buyma_insider/concerns/crawler'
 require_all 'buyma_insider/merchant'
 require_all 'buyma_insider/models'
 require_all 'buyma_insider/persistence'
--- workers/buyma_insider_worker.rb
+++ workers/buyma_insider_worker.rb
@@ -1,9 +1,9 @@
-# $LOAD_PATH.unshift File.expand_path('../lib', File.dirname(__FILE__))
-# $LOAD_PATH.unshift File.expand_path('../config', File.dirname(__FILE__))
-# $LOAD_PATH.unshift File.expand_path('../spec', File.dirname(__FILE__))
+$LOAD_PATH.unshift File.expand_path('../lib', File.dirname(__FILE__))
+$LOAD_PATH.unshift File.expand_path('../config', File.dirname(__FILE__))
+$LOAD_PATH.unshift File.expand_path('../spec', File.dirname(__FILE__))
 
 require 'sidekiq'
-# require 'buyma_insider'
+require 'buyma_insider'
 
 class BuymaInsiderWorker
   include Sidekiq::Worker
