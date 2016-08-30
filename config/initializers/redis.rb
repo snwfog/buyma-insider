b61daa2576e68e5f774d7d73ee1f38382fa86ff1
Charles Yang <donchoa@gmail.com>
2016-08-29 23:23:02 -0400

    Add production readme

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
 require 'redis'
 
-$redis = Redis.new
-$r = $redis unless global_variables.include? :$r
+$redis = Redis.new(host: 'localhost',
+                   port: (ENV[:production] ? 65340 : 6379))
+
+$r     = $redis unless global_variables.include? :$r
