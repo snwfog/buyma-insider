052f63ae45fbde62a1cd0bef4285c30601e42c18
Charles Yang <charles.y.yang@oracle.com>
2016-10-07 17:10:20 -0400

    Some local changes

--- workers/example_worker.rb
+++ workers/example_worker.rb
@@ -3,10 +3,10 @@ $:.unshift(File.expand_path("lib"), File.dirname(__FILE__))
 require 'sidekiq'
 require 'buyma_insider'
 
-class BuymaInsiderWorker
+class ExampleWorker
   include Sidekiq::Worker
 
-  # def perform
-  #   Ssense.new.crawl
-  # end
+  def perform
+    puts "Test"
+  end
 end
