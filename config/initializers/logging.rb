a4ae5668139853f3d4e934229a75c7968b8f9ccc
Charles Yang <donchoa@gmail.com>
2016-10-08 00:12:23 -0400

    Fix additive loggers

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -2,9 +2,8 @@ require 'logging'
 
 Logging.logger.root.level = :info
 Logging.logger.root.add_appenders(Logging.appenders
-                                    .stdout("./log/workers-#{ENV['ENVIRONMENT']}.log",
-                                            :layout => Logging.layouts.pattern(
-                                              :pattern => '[%d] %-5l -- %c : %m\n')))
+                                    .stdout(:layout => Logging.layouts.pattern(
+                                      :pattern => '[%d] %-5l -- %c : %m\n')))
 
 Logging.logger.root.add_appenders(Logging.appenders
                                     .rolling_file("./log/buyma-insider-#{ENV['ENVIRONMENT']}.log",
@@ -13,4 +12,17 @@ Logging.logger.root.add_appenders(Logging.appenders
                                                     :pattern => '[%d] %-5l -- %c : %m\n')))
 
 
+worker          = Logging.logger['Worker']
+worker.level    = :info
+worker.additive = false
+
+worker.add_appenders(Logging.appenders
+                       .stdout(:layout => Logging.layouts.pattern(
+                         :pattern => '[%d] %-5l -- %c : %m\n')))
+
+worker.add_appenders(Logging.appenders
+                       .rolling_file("./log/workers-#{ENV['ENVIRONMENT']}.log",
+                                     :age    => 'weekly',
+                                     :layout => Logging.layouts.pattern(
+                                       :pattern => '[%d] %-5l -- %c : %m\n')))
 
--- workers/startup.rb
+++ workers/startup.rb
@@ -1,22 +1,9 @@
+$:.unshift(File.expand_path("lib"), File.dirname(__FILE__))
+
 require 'sidekiq'
 require 'logging'
 require 'require_all'
 require 'buyma_insider'
 
-worker       = Logging.logger['Worker']
-worker.level = :info
-
-worker.add_appenders(Logging.appenders
-                       .stdout("./log/workers-#{ENV['ENVIRONMENT']}.log",
-                               :layout => Logging.layouts.pattern(
-                                 :pattern => '[%d] %-5l -- %c : %m\n')))
-
-worker.add_appenders(Logging.appenders
-                       .rolling_file("./log/workers-#{ENV['ENVIRONMENT']}.log",
-                                     :age    => 'weekly',
-                                     :layout => Logging.layouts.pattern(
-                                       :pattern => '[%d] %-5l -- %c : %m\n')))
-
-Sidekiq::Logging.logger = worker
 require_all './workers'
 
