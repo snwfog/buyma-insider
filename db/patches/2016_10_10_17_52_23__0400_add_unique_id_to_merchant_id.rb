cc6d236b3c8bc8ef9dfdeca56a90f3b1bd140253
Charles Yang <donchoa@gmail.com>
2016-10-10 19:23:42 -0400

    Fix patches

--- db/patches/2016_10_10_17_52_23__0400_add_unique_id_to_merchant_id.rb
+++ db/patches/2016_10_10_17_52_23__0400_add_unique_id_to_merchant_id.rb
@@ -5,18 +5,18 @@ require 'active_support/core_ext/string/inflections'
 # .eq_join('id', r.table('price_histories'))
 # .filter({ '_type': 'ZaraArticle' })
 r.table('articles')
-  .run($conn).each do |r|
-  if r['id'] =~ /^[a-z]{3}/
-    klazz = r['_type'].safe_constantize
+  .run($conn).each do |record|
+  unless record['id'] =~ /^[a-z]{3}/
+    klazz = record['_type'].safe_constantize
 
     r.table('price_histories')
-      .get(r['id'])
-      .update(article_id: "#{klazz.merchant_code}:#{r['id']}")
+      .get(record['id'])
+      .update(article_id: "#{klazz.merchant_code}:#{record['id']}")
       .run($conn)
 
     r.table('articles')
-      .get(r['id'])
-      .update(id: "#{klazz.merchant_code}:#{r['id']}")
+      .get(record['id'])
+      .update(id: "#{klazz.merchant_code}:#{record['id']}")
       .run($conn)
   end
 end
