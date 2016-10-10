311f862f9d471c6c1b1a11691099f9e2d9e554ef
Charles Yang <donchoa@gmail.com>
2016-10-10 19:26:17 -0400

    Fix setup

--- db/patches/setup.rb
+++ db/patches/setup.rb
@@ -2,6 +2,8 @@ require 'yaml'
 require 'rethinkdb'
 require 'buyma_insider'
 
+raise 'Environment missing' unless ENV.has_key? 'environment'
+
 include RethinkDB::Shortcuts
 db_config  = YAML.load_file(File.expand_path('config/database.yml'))
 env_config = db_config.deep_symbolize_keys[ENV['environment'] || :development]
