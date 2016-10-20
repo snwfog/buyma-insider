08593e2c897749a48ddc8283435f268fd944f82d
Charles Yang <donchoa@gmail.com>
2016-10-19 21:46:56 -0400

    Fix redis cleanup worker

--- app/buyma_insider/workers/scheduled/redis_cleanup_worker.rb
+++ app/buyma_insider/workers/scheduled/redis_cleanup_worker.rb
@@ -1,29 +1,33 @@
 class RedisCleanupWorker < Worker::Base
   recurrence { daily.hour_of_day(6) }
 
-  def perform(opts = { time_limit: Time.now.utc })
+  def perform
+    @opts = { expires_at: Time.now.utc }
+
     $redis.with do |conn|
-      delete_expired_new_articles(opts, conn)
+      delete_expired_new_articles(conn, @opts)
     end
   end
 
   private
-  def delete_expired_new_articles(opts, conn)
-    time_now             = opts[:time_limit]
+  def delete_expired_new_articles(conn, opts)
+    expires_at           = opts[:expires_at]
     expired_articles_ids = conn.zscan_each(:'articles:new:expires_at')
                              .take_while { |article|
       _article_id, expires_at = *article
       expires_at_time         = Time.at(expires_at).utc
-      expires_at_time <= time_now
+      expires_at_time <= expires_at
     }.map(&:first)
 
-    conn.pipelined do
-      conn.zrem(:'articles:new:expires_at', expired_articles_ids)
-      expired_articles_ids
-        .group_by { |article_id| article_id.split(':').first }
-        .each_pair do |merchant_code, articles|
+    unless expired_articles_ids.empty?
+      conn.pipelined do
+        conn.zrem(:'articles:new:expires_at', expired_articles_ids)
+        expired_articles_ids
+          .group_by { |article_id| article_id.split(':').first }
+          .each_pair do |merchant_code, articles|
           conn.hincrby :'articles:new:summary', merchant_code, -articles.count
         end
+      end
     end
   end
 end
