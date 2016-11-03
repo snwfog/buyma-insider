66a45ce641375bd8e8460ba38ec82b9f2981b0e5
Charles Yang <donchoa@gmail.com>
2016-11-03 00:19:32 -0400

    Fix patch

--- db/patches/2016_11_02_23_39_54__0400_fix_previous_patch_with_correct_merchant_id.rb
+++ db/patches/2016_11_02_23_39_54__0400_fix_previous_patch_with_correct_merchant_id.rb
@@ -7,13 +7,13 @@ r.table('articles').run($conn).each do |record|
   merchant_id = record['id'].split(':').first
   print "Updating record:#{record['id']}..."
 
-  if record.has_value? 'merchant_id'
-    puts "Skipping updating merchant_id:#{record['merchant_id']}"
-  else
+  # if record.has_value? 'merchant_id'
+  #   puts "Skipping updating merchant_id:#{record['merchant_id']}"
+  # else
     r.table('articles')
       .get(record['id'])
       .update(merchant_id: merchant_id).run($conn)
     puts "Adding merchant_id:#{merchant_id}..."
-  end
+  # end
 end
 
