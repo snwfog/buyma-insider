de851361a2580f4e54a6ad5398f5dd1b13191e5d
Charles Yang <charles.y.yang@oracle.com>
2017-03-16 23:24:22 -0400

    Shortcut initialization

--- config/initializers/050-slackiq.rb
+++ config/initializers/050-slackiq.rb
@@ -3,8 +3,8 @@ Slackiq.configure worker: ENV['SLACK_WEB_HOOK']
 unless ENV['RACK_ENV'] =~ /prod(uction)?/i
   class << Slackiq
     def notify(opts)
-      logger = Logging.logger[:'Slack Notification']
-      logger.info opts
+      @logger ||= Logging.logger[:'Slack Notification']
+      @logger.info opts
     end
   end
 end
