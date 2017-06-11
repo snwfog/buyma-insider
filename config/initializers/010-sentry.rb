32873bfacdb5da290519bf0c866b108ecbbb43f0
Charles Yang <donchoa@gmail.com>
2017-06-11 15:51:33 -0400

    config

--- config/initializers/010-sentry.rb
+++ config/initializers/010-sentry.rb
@@ -5,5 +5,5 @@ Raven.inject_only :sidekiq
 # :rack-timeout
 Raven.configure do |config|
   config.dsn          = ENV['SENTRY_DSN'] unless BuymaInsider.development?
-  config.environments = [:staging, :production]
+  config.environments = %w[staging production]
 end
