1dec3cddf9686e0a1598f9ec34bea97f4cee32bf
Charles Yang <donchoa@gmail.com>
2017-08-20 11:42:50 -0400

    rates worker

--- app/workers/scheduled/open_exchange_rates_worker.rb
+++ app/workers/scheduled/open_exchange_rates_worker.rb
@@ -7,9 +7,16 @@ class OpenExchangeRatesWorker < Worker::Base
     oer.cache  = proc { |rates_text|
       rates_document = JSON.parse(rates_text)
       rates_document.select! { |k, v| %w(timestamp base rates).include? k }
-      ExchangeRate.create!(rates_document)
+
+      rates_timestamp = rates_document.fetch('timestamp')
+      if ExchangeRate.find_by_timestamp(rates_timestamp)
+        logger.warn "Exchange rates for timestamp: #{rates_timestamp} exists already!"
+      else
+        logger.info "Creating new exchange rates for #{rates_timestamp}."
+        ExchangeRate.create!(rates_document)
+      end
     }
-    
+
     oer.save_rates
   end
 end
