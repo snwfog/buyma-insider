0846f107f9f621c91e11a77237e56af9ad8e2fbc
Charles Yang <charles.y.yang@oracle.com>
2017-08-28 16:25:51 -0400

    run gc before fork

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,3 +18,7 @@ listen            "#{app_dir}/tmp/sockets/unicorn.sock", backlog: 64
 stderr_path       "#{app_dir}/log/unicorn-stderr.log"
 stdout_path       "#{app_dir}/log/unicorn-stdout.log"
 
+before_fork do |server, worker|
+  # get rid of rubbish
+  GC.start
+end
