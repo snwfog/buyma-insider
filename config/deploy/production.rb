cd98c31982b2200d7f1302bc9422a5aae211e202
Charles Yang <donchoa@gmail.com>
2017-08-29 18:32:06 -0400

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,10 @@
 set :deploy_to, '/usr/local/var/www/buyma_insider'
-server ENV['PRODUCTION_SSH_SERVE'], user:  ENV['PRODUCTION_SSH_USER'],
-                                    roles: :all
+server ENV['PRODUCTION_SSH_SERVER'],
+       user:  ENV['PRODUCTION_SSH_USER'],
+       roles: :all
+
+set :hostname, 'mini'
+set :port_number, 8080
 
 # Elasticsearch path, where to copy scripts and settings
 set :elasticsearch_path, '/usr/local/etc/'
