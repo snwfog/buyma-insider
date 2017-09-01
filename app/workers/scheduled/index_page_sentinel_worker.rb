acb68e9cdbda3471c76b5180b63090927a89aa2b
Charles Yang <donchoa@gmail.com>
2017-09-01 18:22:14 -0400

    fix workerg

--- app/workers/scheduled/index_page_sentinel_worker.rb
+++ app/workers/scheduled/index_page_sentinel_worker.rb
@@ -8,25 +8,25 @@ class IndexPageSentinelWorker < Worker::Base
     root_indices = IndexPage.includes(:merchant).root.all
     slack_notify(text: ":japanese_ogre: *Sentinel Started*\nChecking #{root_indices.count} root index pages")
     all_statuses = root_indices.map do |index_page|
-      check_index_page_health(index_page)
+      get_index_page_status(index_page)
     end
-    
+
     logger.info JSON.pretty_generate(all_statuses)
     statuses_grouped_by = all_statuses
                             .sort_by!(&:http_status)
                             .group_by do |index_page_status|
-      http_status_category(index_page_status[:http_status])
+      http_status_category(index_page_status.http_status)
     end
-    
+
     # TODO: Clunky, change this later
     slack_notify(sentinel_slack_report(statuses_grouped_by))
   end
-  
+
   private
   def sentinel_slack_report(statuses_grouped_by)
     colors = { :'2xx' => '008000', :'3xx' => 'FFD700',
                :'4xx' => 'FF0000', :'5xx' => 'C71585' }
-    
+
     Hash[:text, ':japanese_ogre: *Sentinel Report*'].tap do |report|
       report[:attachments] ||= []
       statuses_grouped_by.each do |status, index_page_statuses|
@@ -39,8 +39,8 @@ class IndexPageSentinelWorker < Worker::Base
       end
     end
   end
-  
-  def check_index_page_health(index_page)
+
+  def get_index_page_status(index_page)
     begin
       merchant     = index_page.merchant
       raw_response = fetch_uri(index_page.full_url)
@@ -52,16 +52,16 @@ class IndexPageSentinelWorker < Worker::Base
     rescue RestClient::ExceptionWithResponse => ex
       raw_response = ex.response
     end
-    
+
     Hashie::Mash.new(index_page:     index_page.full_url,
                      http_status:    raw_response.try(:code),
                      size_in_kb:     file_size_in_kb || 0.0,
                      redirection:    raw_response.try(:history).try(:any?),
                      articles_count: article_nodes.try(:count))
   end
-  
+
   def http_status_category(status)
-    case status[:http_status]
+    case status
     when 500..599
       :'5xx'
     when 400..499
