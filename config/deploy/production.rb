254a2968d5b67a1be6c305cec094c8d9f809cec8
Charles Yang <donchoa@gmail.com>
2017-09-06 23:39:18 -0400

    capistrano automated tasks

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,38 @@ server ENV['PRODUCTION_SSH_SERVER'], user: ENV['PRODUCTION_SSH_USER'], roles: :a
 
 set :hostname, 'mini'
 set :port_number, 8080
+
+set :default_env, { PATH: '/usr/local/bin:$PATH' }
+
+after 'deploy:published', 'flush_redis'
+after 'deploy:published', 'setup_env'
+after 'deploy:published', 'reload_monit'
+
+desc 'Flush redis cache'
+task :flush_redis do
+  on roles(:all) do |host|
+    # capture(execute(:env))
+    # capture(execute(:bundle, :env))
+    execute :'redis-cli', '-n 1', 'flushdb'
+    execute :'redis-cli', '-n 0', 'del static:bootstrap'
+  end
+end
+
+desc 'Copy production dotenv to app path'
+task :setup_env do
+  on roles(:all) do
+    within shared_path do
+      execute :cp, 'dotenv', "#{current_path}/.env"
+    end
+  end
+end
+
+desc 'Copy monitrc to home dir and reload'
+task :reload_monit do
+  on roles(:all) do
+    within current_path do
+      execute :cp, '.monitrc', '~/.monitrc'
+      execute :monit, :reload
+    end
+  end
+end
