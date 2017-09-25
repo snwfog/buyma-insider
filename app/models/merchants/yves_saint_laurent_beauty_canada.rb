50c5ef22a570fe86ff7578296407221590825998
Charles Yang <donchoa@gmail.com>
2017-09-24 23:19:07 -0400

    allow paging for ysl

--- app/models/merchants/yves_saint_laurent_beauty_canada.rb
+++ app/models/merchants/yves_saint_laurent_beauty_canada.rb
@@ -2,7 +2,27 @@ module Merchants
   module YvesSaintLaurentBeautyCanada
     def query_strings
       { format: :ajax,
-        sz:     1000 } # page size, looks like it works with large number
+        sz:     120 } # page size, looks like it works with large number
+    end
+
+    def extract_index_pages!(root_index_page)
+      pager_node = super
+      if pager_node.nil?
+        []
+      else
+        # grab largest pages and split
+        href                = pager_node.css('ul li.pagination_list_item a.pagination_list_link').last['href']
+        uri                 = URI(href)
+        query_strings       = Rack::Utils.parse_nested_query(uri.query)
+        articles_per_page   = query_strings['sz'].to_i
+        last_page_starts_at = query_strings['start'].to_i
+        max_articles        = articles_per_page + last_page_starts_at - 1
+
+        (0..max_articles).step(120) do |page_starts_at|
+          relative_path = root_index_page.relative_path + "?start=#{page_starts_at}"
+          root_index_page.index_pages.build(relative_path: relative_path, merchant: self)
+        end
+      end
     end
 
     def extract_attrs!(node)
@@ -15,7 +35,7 @@ module Merchants
 
       { sku:         product_sku,
         name:        product_name,
-        description: product_description,
+        description: product_description.blank? ? product_name.capitalize : product_description,
         link:        "//#{product_link.host}#{product_link.path}",
         price:       product_price }
     end
